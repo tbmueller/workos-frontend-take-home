1965ce53800512aec384af17b445e81f72019462
Trista (tbmueller) <trista.mueller@gmail.com>
2025-03-30 14:07:00 -0400

    Get not found page looking nice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,24 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { Users } from './users'
 import { Roles } from './roles'
 import { NotFound } from './not-found'
+import { Container } from '@radix-ui/themes'
 
 function App() {
 
   return (
-    <>
+    <Container
+      size={{
+        initial: "2",
+        lg: "4"
+      }}
+    >
       <Routes>
         <Route index element={<Navigate to="/users" />}></Route>
         <Route path="/users" element={<Users/>} />
         <Route path="/roles" element={<Roles />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </Container>
   )
 }
 
--- client/src/not-found/index.tsx
+++ client/src/not-found/index.tsx
@@ -1,3 +1,17 @@
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
+import { Callout } from "@radix-ui/themes"
+import { useLocation } from "react-router-dom"
+
 export const NotFound = () => {
-    return "Not found";
+    const location = useLocation();
+    return <>
+        <Callout.Root color="red">
+            <Callout.Icon>
+                <ExclamationTriangleIcon />
+            </Callout.Icon>
+            <Callout.Text>
+                Error: Route {location.pathname} not found.
+            </Callout.Text>
+        </Callout.Root>
+    </>
 }
